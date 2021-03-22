4db871777824a3d96e61ff4f6a69e5c1d45704ca
mukul <mukul@mukuls-MacBook-Pro.local>
2021-03-22 19:32:42 +0530

    hello world program

---
+++ hello_world/main.py
@@ -0,0 +1,9 @@
+print('-------------------------------')
+print('        Hello App')
+print('-------------------------------')
+print()
+
+# TODO
+user_text = input('What is your name? ')
+greeting = 'Nice to meet you '
+print(greeting + user_text)
