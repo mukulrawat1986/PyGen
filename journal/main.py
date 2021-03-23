2d7043f2d5ad022727da99125897bd225086921a
mukulrawat1986 <mukulsrawat@gmail.com>
2021-03-23 19:58:47 +0530

    create skeleton

---
+++ journal/main.py
@@ -0,0 +1,29 @@
+def main():
+    print_header()
+    run_event_loop()
+
+
+def print_header():
+    print('----------------------------')
+    print('     JOURNAL APP')
+    print('----------------------------')
+
+
+def run_event_loop():
+    print('What do you want to do with your journal?')
+
+    while 1:
+        cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ').lower().strip()
+        if cmd == 'l':
+            print("L")
+        elif cmd == 'a':
+            print("A")
+        elif cmd == 'x':
+            break
+        else:
+            print("Sorry, we don't understand '{}'".format(cmd))
+
+    print('Done, goodbye.')
+
+
+main()
