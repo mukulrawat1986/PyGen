6d4f16135339f83e919b7cff59f1efc4bd4ebfd2
mukulrawat1986 <mukulsrawat@gmail.com>
2021-03-30 21:35:40 +0530

    write function to get state and city from the zipcode

--- weather_client/main.py
+++ weather_client/main.py
@@ -1,3 +1,6 @@
+import urllib.request
+import json
+
 
 def main():
 	# print the header
@@ -23,8 +26,13 @@ def print_the_header():
 	print()
 
 
-def get_state_and_city(zicode):
-	return "MA", "BRIMFIELD"
+def get_state_and_city(zipcode):
+	url = 'http://ziptasticapi.com/{}'.format(zipcode)
+	# set a known browser user agent
+	req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+	f = urllib.request.urlopen(req)
+	data = json.loads(f.read().decode())
+	return data['state'], data['city']
 
 
 def get_html_from_web(state, city, zipcode):
